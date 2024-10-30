240191115a3bd6a49448cbbeb10b77eece520b79
dhkang <dhkang960912@gmail.com>
2024-10-30 13:52:17 +0900

    [INIT] 폰트 설정

--- frontend/aptrue/src/app/layout.tsx
+++ frontend/aptrue/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "@/styles/globals.css";
 import RecoilRootProvider from "@/utils/RecoilRootProvider";
 
 export const metadata: Metadata = {

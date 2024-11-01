516e83fc57a76f174b85a37ff4b695161bb6e95f
dhkang <dhkang960912@gmail.com>
2024-11-01 17:05:43 +0900

    [Feat] #14 cctv 페이지 링크 넣기

---
+++ frontend/aptrue/src/app/(webProtected)/(web)/cctv/layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode, Suspense } from 'react';
+import WebNav from '@/components/common/navbar/WebNav';
+import Headerbar from '@/components/common/header/Headerbar';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Headerbar name="차은우" />
+        <WebNav />
+      </Suspense>
+
+      {children}
+    </div>
+  );
+}

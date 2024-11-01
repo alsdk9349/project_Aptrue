1005f9a747ab27de41c163ad8faf2c253ca4622c
유지연 <apple20011215@gmail.com>
2024-11-01 11:31:20 +0900

    [Feat] common/#4 인풋컴포넌트 제작

--- frontend/aptrue/src/app/page.tsx
+++ frontend/aptrue/src/app/page.tsx
@@ -1,6 +1,38 @@
+"use client"
+
 import style from '@/app/page.module.scss';
+import GeneralInput from '@/components/common/input/GeneralInput';
+import TimeInput from '@/components/common/input/TimeInput';
+import LoginInput from '@/components/common/input/LoginInput';
+
 export default function Home() {
-  return <div className={style.container}>Home</div>;
+
+  const changeName = (name:string) => {
+    console.log(name)
+  }
+
+  const changeEmail = (email:string) =>  {
+    console.log(email)
+  }
+
+  const handleTime = (time:string) => {
+    console.log(time)
+  }
+
+  const handleId = (id:string) => {
+    console.log(id)
+  }
+
+  return (
+    <>
+    <div className={style.container}>Home</div>
+    <GeneralInput label="이름" placeholder='홍길동' size='short' onChange={changeName}/>
+    <GeneralInput label="이메일" placeholder='apple12345@gamil.com' size="long" onChange={changeEmail}/> 
+    <TimeInput onChange={handleTime} isWeb={true} />
+    <TimeInput onChange={handleTime} isWeb={false} />
+    <LoginInput label='아이디' onChange={handleId} placeholder='아이디를 입력하세요'/>
+    </>
+  );
 }
 
 

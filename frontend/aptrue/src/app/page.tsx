f179c4c02c5b85722674113a3ff142a309a47e50
dhkang <dhkang960912@gmail.com>
2024-11-01 14:14:52 +0900

    [Feat] #7 페이지네이션 세팅

--- frontend/aptrue/src/app/page.tsx
+++ frontend/aptrue/src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 
 import style from '@/app/page.module.scss';
 import Button from '@/components/common/button/Button';
@@ -6,40 +6,53 @@ import Button from '@/components/common/button/Button';
 import GeneralInput from '@/components/common/input/GeneralInput';
 import TimeInput from '@/components/common/input/TimeInput';
 import LoginInput from '@/components/common/input/LoginInput';
+import Pagination from '@/components/common/pagination/pagination';
 
 export default function Home() {
+  const changeName = (name: string) => {
+    console.log(name);
+  };
 
-  const changeName = (name:string) => {
-    console.log(name)
-  }
+  const changeEmail = (email: string) => {
+    console.log(email);
+  };
 
-  const changeEmail = (email:string) =>  {
-    console.log(email)
-  }
+  const handleTime = (time: string) => {
+    console.log(time);
+  };
 
-  const handleTime = (time:string) => {
-    console.log(time)
-  }
-
-  const handleId = (id:string) => {
-    console.log(id)
-  }
+  const handleId = (id: string) => {
+    console.log(id);
+  };
 
   return (
-    <>
-    (
-    <>
+    <div className={style.container}>
+      <div>Home</div>
       <Button color="white" size="webBig" clickedColor="blue">
         하이
       </Button>
-      <div className={style.container}>Home</div>
-    </>
-  )
-    <GeneralInput label="이름" placeholder='홍길동' size='short' onChange={changeName}/>
-    <GeneralInput label="이메일" placeholder='apple12345@gamil.com' size="long" onChange={changeEmail}/> 
-    <TimeInput onChange={handleTime} isWeb={true} />
-    <TimeInput onChange={handleTime} isWeb={false} />
-    <LoginInput label='아이디' onChange={handleId} placeholder='아이디를 입력하세요'/>
-    </>
+
+      <Pagination></Pagination>
+
+      <GeneralInput
+        label="이름"
+        placeholder="홍길동"
+        size="short"
+        onChange={changeName}
+      />
+      <GeneralInput
+        label="이메일"
+        placeholder="apple12345@gamil.com"
+        size="long"
+        onChange={changeEmail}
+      />
+      <TimeInput onChange={handleTime} isWeb={true} />
+      <TimeInput onChange={handleTime} isWeb={false} />
+      <LoginInput
+        label="아이디"
+        onChange={handleId}
+        placeholder="아이디를 입력하세요"
+      />
+    </div>
   );
 }

725f4e1d3311b385dfe13b1cf81d918f5be2e46a
dhkang <dhkang960912@gmail.com>
2024-11-04 17:46:32 +0900

    [Feat] #14 CCTV 처리 목록 퍼블리싱 최종 완료, 페이지네이션 url Path 추가

--- frontend/aptrue/src/app/(webProtected)/(web)/page.tsx
+++ frontend/aptrue/src/app/(webProtected)/(web)/page.tsx
@@ -28,7 +28,6 @@ export default function Home() {
 
   return (
     <div className={style.container}>
-      <Pagination pageProps=""></Pagination>
       <Button color="lightBlue" size="webSmall">
         하이
       </Button>
--- frontend/aptrue/src/components/common/pagination/Pagination.tsx
+++ frontend/aptrue/src/components/common/pagination/Pagination.tsx
@@ -3,15 +3,16 @@ import { useRouter } from 'next/navigation';
 import style from './Pagination.module.scss';
 
 interface PageProps {
-  pageProps: string;
+  pageNum: string;
+  urlPath: string;
 }
 
 const ITEMS_PER_PAGE = 10;
 const BUTTONS_PER_GROUP = 5;
 
-export default function Pagination({ pageProps }: PageProps) {
+export default function Pagination({ urlPath, pageNum }: PageProps) {
   const router = useRouter();
-  const page = parseInt(pageProps, 10) || 1;
+  const page = parseInt(pageNum, 10) || 1;
   // 페이지네이션 버튼 생성
   const currentGroup = Math.floor((page - 1) / BUTTONS_PER_GROUP);
   const startPage = currentGroup * BUTTONS_PER_GROUP;
@@ -23,20 +24,18 @@ export default function Pagination({ pageProps }: PageProps) {
 
   // 페이지 이동 함수
   const handlePageChange = (newPage: number) => {
-    router.push(`/clip/${newPage}`);
+    router.push(`${urlPath}/${newPage}`);
   };
 
   return (
     <div className={style.container}>
       {/* 이전 그룹 버튼 */}
-      {currentGroup > 0 && (
-        <button
-          onClick={() => handlePageChange(startPage - BUTTONS_PER_GROUP)}
-          className={style.group}
-        >
-          &lt; &nbsp; Back
-        </button>
-      )}
+      <button
+        onClick={() => handlePageChange(startPage - BUTTONS_PER_GROUP)}
+        className={`${style.group} ${currentGroup > 0 ? '' : style.hidden}`}
+      >
+        &lt; &nbsp; Back
+      </button>
       {/* 페이지네이션 버튼 */}
       {pageNumbers.map((pageNumber) => (
         <button

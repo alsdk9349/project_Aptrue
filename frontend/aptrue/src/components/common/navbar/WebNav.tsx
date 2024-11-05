b3387608b526afb55b77f92df052cd44a940e0da
전가현 <rkgus678@gmail.com>
2024-11-05 12:18:20 +0900

    [Chore] #23 cctv 처리 링크 주석처리

--- frontend/aptrue/src/components/common/navbar/WebNav.tsx
+++ frontend/aptrue/src/components/common/navbar/WebNav.tsx
@@ -47,7 +47,7 @@ export default function WebNav() {
           </Link>
 
           <Link
-            href="/cctv"
+            href="/" //[*]나중에 cctv 링크로 수정 필요
             className={`${style.cctv} ${style.commonItem} ${
               activeItem === 'cctv' ? style.active : ''
             }`}

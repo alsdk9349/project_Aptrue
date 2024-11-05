884f4c980a02771477da98300b7858e363c1f554
전가현 <rkgus678@gmail.com>
2024-11-05 16:49:11 +0900

    [Feat] #24 구역별 cctv api 코드 작성

--- frontend/aptrue/src/components/homePage/CCTVHome.tsx
+++ frontend/aptrue/src/components/homePage/CCTVHome.tsx
@@ -1,9 +1,112 @@
-'use client';
+// 'use client';
 
-import CCTVScreen from './CCTV/CCTVScreen';
+// import CCTVScreen from './CCTV/CCTVScreen';
+// import CCTVButton from './CCTVButton';
+// import style from './CCTVHome.module.scss';
+// import { useEffect, useState } from 'react';
+// import { useRouter } from 'next/navigation';
+
+// const cctvZone = [
+//   '101동 주변',
+//   '102동 주변',
+//   '101동 주차장',
+//   '102동 주차장',
+//   '아파트 입구',
+//   '정문 경비실',
+//   '후문 경비실',
+//   '지하 스포츠센터',
+//   '정문 어린이집',
+// ];
+
+// const videoUrls = [
+//   '/videos/entrance.mp4',
+//   '/videos/park2.mp4',
+//   '/videos/park.mp4',
+//   '/videos/playground.mp4',
+// ];
+
+// export default function CCTVHome() {
+//   const router = useRouter();
+
+//   // //[todo] 구역별 cctv 조회 api
+//   // useEffect(() => {
+//   //   const fetchVideo = async () => {
+//   //     const response = await fetch(
+//   //       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/api/cctv/get/${activeZone}`,
+//   //     );
+//   //     if (!response.ok) {
+//   //       return <div>오류가 발생했습니다...</div>;
+//   //     }
+//   //     const data = await response.json();
+//   //     const videoUrls = data.data.map(
+//   //       (item: { camId: number; cctvUrl: string }) => item.cctvUrl,
+//   //     );
+//   //     setRandomVideos(videoUrls);
+//   //   };
+
+//   //   fetchVideo();
+//   // }, [activeZone]);
+
+//   // const handleZoneChange = (zone: string) => {
+//   //   // setActiveZone(zone);
+//   //   // const shuffledVideos = [...videoUrls].sort(() => Math.random() - 0.5);
+//   //   // setRandomVideos(shuffledVideos);
+//   //   router.push(`/${zone}`)
+//   // };
+
+//   // const openModal = (videoUrl: string) => {
+//   //   setCurrentVideoUrl(videoUrl);
+//   //   setIsModalOpen(true);
+//   // };
+
+//   // const closeModal = () => {
+//   //   setIsModalOpen(false);
+//   //   setCurrentVideoUrl('');
+//   // };
+
+//   return (
+//     <div>
+//       <div className={style.button}>
+//         <CCTVButton
+//           activeZone={activeZone}
+//           setActiveZone={handleZoneChange}
+//           cctvZone={cctvZone}
+//         />
+//       </div>
+//       <div className={style.cctvContainer}>
+//         {randomVideos.map((videoUrl, index) => (
+//           <CCTVScreen
+//             key={index}
+//             activeZone={activeZone}
+//             camNumber={index + 1}
+//             videoUrl={videoUrl}
+//             // onClick={() => openModal(videoUrl)}
+//           />
+//         ))}
+//       </div>
+//       {/* {isModalOpen && (
+//         <div className={style.modal} onClick={closeModal}>
+//           <div
+//             className={style.modalContent}
+//             onClick={(e) => e.stopPropagation()}
+//           >
+//             <video src={currentVideoUrl} controls={false} autoPlay loop muted />
+//           </div>
+//         </div>
+//       )} */}
+//     </div>
+//   );
+// }
+
+// 서버 컴포넌트
 import CCTVButton from './CCTVButton';
+import CCTVScreen from './CCTV/CCTVScreen';
 import style from './CCTVHome.module.scss';
-import { useState } from 'react';
+
+interface CCTVHomeProps {
+  activeZone: string;
+  videoUrls: string[];
+}
 
 const cctvZone = [
   '101동 주변',
@@ -17,65 +120,22 @@ const cctvZone = [
   '정문 어린이집',
 ];
 
-const videoUrls = [
-  '/videos/entrance.mp4',
-  '/videos/park2.mp4',
-  '/videos/park.mp4',
-  '/videos/playground.mp4',
-];
-
-export default function CCTVHome() {
-  const [activeZone, setActiveZone] = useState<string>('101동 주변');
-  const [randomVideos, setRandomVideos] = useState<string[]>(videoUrls);
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const [currentVideoUrl, setCurrentVideoUrl] = useState<string>('');
-
-  const handleZoneChange = (zone: string) => {
-    setActiveZone(zone);
-    const shuffledVideos = [...videoUrls].sort(() => Math.random() - 0.5);
-    setRandomVideos(shuffledVideos);
-  };
-
-  const openModal = (videoUrl: string) => {
-    setCurrentVideoUrl(videoUrl);
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setCurrentVideoUrl('');
-  };
-
+export default function CCTVHome({ activeZone, videoUrls }: CCTVHomeProps) {
   return (
     <div>
       <div className={style.button}>
-        <CCTVButton
-          activeZone={activeZone}
-          setActiveZone={handleZoneChange}
-          cctvZone={cctvZone}
-        />
+        <CCTVButton activeZone={activeZone} cctvZone={cctvZone} />
       </div>
       <div className={style.cctvContainer}>
-        {randomVideos.map((videoUrl, index) => (
+        {videoUrls.map((videoUrl, index) => (
           <CCTVScreen
             key={index}
             activeZone={activeZone}
             camNumber={index + 1}
             videoUrl={videoUrl}
-            onClick={() => openModal(videoUrl)} // 클릭 핸들러 추가
           />
         ))}
       </div>
-      {isModalOpen && (
-        <div className={style.modal} onClick={closeModal}>
-          <div
-            className={style.modalContent}
-            onClick={(e) => e.stopPropagation()}
-          >
-            <video src={currentVideoUrl} controls={false} autoPlay loop muted />
-          </div>
-        </div>
-      )}
     </div>
   );
 }

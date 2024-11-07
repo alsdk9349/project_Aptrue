15e391a91d260a46c108e84d24c4aeee636d91ce
유지연 <apple20011215@gmail.com>
2024-11-07 14:16:23 +0900

    [Feat] admin/#41 로그인 수정

--- frontend/aptrue/src/auth.d.ts
+++ frontend/aptrue/src/auth.d.ts
@@ -1,26 +1,5 @@
 import NextAuth, {DefaultSession, User} from 'next-auth';
 
-// interface Admin {
-//     adminId: number;
-//     name: string;
-//     account: string;
-//     password:string;
-//     phone:string;
-//     createdAt:string;
-// }
-
-// interface postAdmin {
-//     account:string;
-//     name:string;
-//     password:string;
-//     phone:string;
-// }
-
-// interface postLogin {
-//     account:string;
-//     password:string;
-// }
-
 declare module 'next-auth' {
     // User 인터페이스는 authorize 함수가 로그인 성공 시 반환하는 사용자 정보를 정의
     interface User {
@@ -32,7 +11,8 @@ declare module 'next-auth' {
     interface Session {
       accessToken: string
     }
-  }
+}
+
 declare module '@auth/core/jwt' {
     interface JWT {
       accessToken: string

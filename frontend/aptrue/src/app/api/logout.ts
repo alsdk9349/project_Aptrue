303916ba22d18f4ae585664d93c2293f21bebdf4
유지연 <apple20011215@gmail.com>
2024-11-08 15:11:03 +0900

    [Feat] admin/#45 로그아웃

---
+++ frontend/aptrue/src/app/api/logout.ts
@@ -0,0 +1,7 @@
+import { NextApiResponse, NextApiRequest } from 'next';
+
+export default function handler(req:NextApiRequest, res:NextApiResponse) {
+    // 로그아웃 요청 시 accessToken 쿠키 제거
+    res.setHeader('Set-Cookie', 'accessToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT;');
+    res.status(200).json({ message: '로그아웃 성공' });
+}

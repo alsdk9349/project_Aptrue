c1fdaf4f146abd9568e43fe1f8fe1405b4ac2ecf
ImSeongHun <daltjdgns111@naver.com>
2024-11-10 15:11:02 +0900

    fix : jenkins 경로 추가

--- backend/src/main/java/aptrue/backend/Global/Config/SecurityConfig.java
+++ backend/src/main/java/aptrue/backend/Global/Config/SecurityConfig.java
@@ -83,6 +83,7 @@ public class SecurityConfig {
                             .requestMatchers("/api/admin/**").permitAll()
                             .requestMatchers("/api/clip/detail/**").permitAll()
                             .requestMatchers("/api/**").permitAll()
+                            .requestMatchers("/jenkins/**").permitAll()
                             .anyRequest().authenticated(); // 그 외 경로는 인증 필요
                 });
 

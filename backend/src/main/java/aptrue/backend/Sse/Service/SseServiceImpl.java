5546f2c4e60fcd56f2bea030fc1e639e6d13850f
ImSeongHun <daltjdgns111@naver.com>
2024-11-13 17:34:22 +0900

    fix : 캐시도ã된 이벤트 저ã전송 주석

--- backend/src/main/java/aptrue/backend/Sse/Service/SseServiceImpl.java
+++ backend/src/main/java/aptrue/backend/Sse/Service/SseServiceImpl.java
@@ -36,18 +36,18 @@ public class SseServiceImpl implements SseService {
         sendEvent("연결 성공1", responseDto);
 
         // 캐시된 이벤트 전송
-        if (!sseRepository.getAllEmitters().isEmpty()) {
-            sseRepository.getCachedEvents(clientId).forEach(eventWrapper -> {
-                try {
-                    emitter.send(SseEmitter.event()
-                            .name(eventWrapper.getEventName())
-                            .data(eventWrapper.getData()));
-                } catch (Exception e) {
-                    log.error("Failed to send cached event to client {}: {}", clientId, e.getMessage());
-                }
-            });
-            sseRepository.clearCachedEvents(clientId);
-        }
+//        if (!sseRepository.getAllEmitters().isEmpty()) {
+//            sseRepository.getCachedEvents(clientId).forEach(eventWrapper -> {
+//                try {
+//                    emitter.send(SseEmitter.event()
+//                            .name(eventWrapper.getEventName())
+//                            .data(eventWrapper.getData()));
+//                } catch (Exception e) {
+//                    log.error("Failed to send cached event to client {}: {}", clientId, e.getMessage());
+//                }
+//            });
+//            sseRepository.clearCachedEvents(clientId);
+//        }
 
         return emitter;
     }

1c6e5c120b633271ad3ccc794cd5bd750420b5c2
유지연 <apple20011215@gmail.com>
2024-11-17 15:08:34 +0900

    [Feat] admin/#76 로그아웃 수정

--- frontend/aptrue/src/app/api/logout/route.ts
+++ frontend/aptrue/src/app/api/logout/route.ts
@@ -1,56 +1,29 @@
 // src/app/api/logout/route.ts
 
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 
-export async function POST(req: NextRequest) {
-    console.log('로그아웃 요청 수신');
+export async function POST() {
+
+    console.log('로그아웃 route.ts 도착')
 
     const response = NextResponse.json(
         { message: '로그아웃 성공' },
         { status: 200 }
     );
 
-    const cookies = [
-        'accessToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Domain=k11c101.p.ssafy.io; Secure; HttpOnly; SameSite=None;',
-        'refreshToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Domain=k11c101.p.ssafy.io; Secure; HttpOnly; SameSite=None;',
-    ];
+    response.cookies.set('accessToken', '', {maxAge:-1})
+    response.cookies.set('refreshToken', '', {maxAge:-1})
 
-    cookies.forEach((cookie) => {
-        response.headers.append('Set-Cookie', cookie);
-    });
+    // const cookies = [
+    //     'accessToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Domain=k11c101.p.ssafy.io; Secure; HttpOnly; SameSite=None;',
+    //     'refreshToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Domain=k11c101.p.ssafy.io; Secure; HttpOnly; SameSite=None;',
+    // ];
+
+    // cookies.forEach((cookie) => {
+    //     response.headers.append('Set-Cookie', cookie);
+    // });
 
     return response;
 }
-
-
-
-
-// export const dynamic = 'force-dynamic';
-
-// import { NextResponse } from 'next/server';
-// import type { NextRequest } from 'next/server';
-
-// export async function POST(req: NextRequest) {
-//     console.log('로그아웃 요청 수신');
-
-//     const response = NextResponse.json(
-//         { message: '로그아웃 성공' },
-//         { status: 200 }
-//     );
-
-//     const cookies = [
-//         'accessToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure; SameSite=Strict;',
-//         'refreshToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure; SameSite=Strict;',
-//     ];
-
-//     cookies.forEach((cookie) => {
-//         response.headers.append('Set-Cookie', cookie);
-//     });
-
-//     return response;
-// }
-
-
 
 
